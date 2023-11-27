92bf02da31ec7dfce811252390ddd78f180c35cf
DESKTOP-4UMPI50\KGA <tkdtkddmlehdanf97@gmail.com>
2023-11-27 21:22:49 +0900

    김형준 : [Fix] 트램펄린 코루틴 탈출조건 (수정중)

--- Metalord/Assets/_Test/KHJ/Scripts/InteractableObject/Trampoline.cs
+++ Metalord/Assets/_Test/KHJ/Scripts/InteractableObject/Trampoline.cs
@@ -1,15 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Trampoline : MonoBehaviour, IInteractableObject
+public class Trampoline : MonoBehaviour
 {
-    public void Interact()
+
+    private Vector3 originSize = default;
+    public Vector3 downSize = default;
+
+    public bool isPlay = false;
+
+    private void Start()
     {
-        Debug.Log("트램펄린 Interact 메서드 실행됨");
+        isPlay = false;
+        originSize = transform.localScale;
+        downSize = new Vector3(transform.localScale.x, transform.localScale.y - 2f, transform.localScale.z);
     }
 
-    public void InteractOut()
+
+    private void OnTriggerStay(Collider other)
     {
+        if(other.tag == "Player")
+        {
+            CapsuleCollider capsuleCollider = other.gameObject.GetComponent<CapsuleCollider>();
+            PhysicMaterial otherPhysicMat = capsuleCollider.material;
+            otherPhysicMat.bounciness = 0.9f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CapsuleCollider capsuleCollider = other.gameObject.GetComponent<CapsuleCollider>();
+            PhysicMaterial otherPhysicMat = capsuleCollider.material;
+            otherPhysicMat.bounciness = 0f;
+        }
+    }
+
+    IEnumerator ScaleDown()
+    {
+        float duration = Time.deltaTime * 10f;
+        //for (float i = transform.localScale.y; i >= downSize.y; i--)
+        //{
+
+        //    //else if ( isPlay  == true) 
+        //    //{
+        //    //    yield break;
+        //    //}
+
+        //    transform.localScale = Vector3.Lerp(transform.localScale, downSize, duration);
+        //    yield return null;
+            
+        //}
+        while ( transform.localScale.y > downSize.y ) 
+        {            
+            //Debug.Log("트램펄린 업 실행됨?");
+            transform.localScale = Vector3.Lerp(transform.localScale, downSize, duration);
+            yield return null;
+            Debug.Log("여기 ㅇ무슨 ㅅ문데");
+            if (transform.localScale.y <= downSize.y)
+            {
+                Debug.Log("브레이크");
+                transform.localScale = downSize;
+                isPlay = true;
+                yield break;
+            }
+
+        }
+    }
+
+
+    IEnumerator ScaleUp()
+    {
+        float duration = Time.deltaTime;
+        //for(float i = downSize.y; i <= originSize.y; i++)
+        //{
+        //    Debug.Log("트램펄린 업 실행됨?");
+        //    transform.localScale = Vector3.Lerp(transform.localScale, originSize, duration);
+        //    yield return null;
+        //}
+        while (transform.localScale.y <= originSize.y)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originSize, duration);
+            yield return null;
+        }
+
+        isPlay = false;
+        if (transform.localScale.y >= originSize.y)
+        {
+           
+            transform.localScale = originSize;
+            yield break;
+        }
+    }
+
+
+    public void TouchPad()
+    {
+        Debug.Log(isPlay);
+        if (isPlay == false)
+        {
+            StartCoroutine(ScaleDown());
+        }
+    }
+
+    public void ChangeOriginSize()
+    {
+        Debug.Log(isPlay);
+
+        if (isPlay == true)
+        {
+            StartCoroutine(ScaleUp());
+        }
     }
 }
