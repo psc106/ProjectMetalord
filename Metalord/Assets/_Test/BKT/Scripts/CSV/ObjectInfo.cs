447aaebd597daddc492ba638230f5e82d6424810
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-11-30 15:44:23 +0900

    배경택: [Add] 도감 정보 반영을 위한 기본 CSV 적용

---
+++ Metalord/Assets/_Test/BKT/Scripts/CSV/ObjectInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 도감 오브젝트의 정보_CSV를 통해 반영
+/// 231130_배경택
+/// </summary>
+public class ObjectInfo : MonoBehaviour
+{
+    public string objectID;
+    public string objectPlace;
+    public string objectName;
+    public string objectDescription;
+}
