593a85a4bf368bae7e45a22fdb0dee803eb1da30
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-07 13:59:09 +0900

    배경택: [Add] 메인옵션(환경설정) UI 기본 베이스 제작

--- Metalord/Assets/_Test/BKT/Scripts/TestController.cs
+++ Metalord/Assets/_Test/BKT/Scripts/TestController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class TestController : MonoBehaviour
 {
     public GameObject recordUI; // 여기에 UI GameObject를 연결합니다.
-    private bool isUIVisible = false; // UI의 현재 상태를 추적합니다.
+    public GameObject settingUI;
+    private bool isRecordUIVisible = false; 
+    private bool isSettingUIVisible = false;
 
     private void Start()
     {
@@ -16,22 +18,37 @@ public class TestController : MonoBehaviour
 
     void Update()
     {
-
+        // 수집 도감
         if (Input.GetKeyDown(KeyCode.E))
         {
             // E 키를 누르면 UI를 켜기 or 끄기
-            isUIVisible = !isUIVisible;
-            recordUI.SetActive(isUIVisible);
+            isRecordUIVisible = !isRecordUIVisible;
+            recordUI.SetActive(isRecordUIVisible);
+        }
+
+        // 환경설정창
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // W 키를 누르면 UI를 켜기 or 끄기
+            isSettingUIVisible = !isSettingUIVisible;
+            settingUI.SetActive(isSettingUIVisible);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {           
             // ESC 키를 누르면 UI 끄기
-            if (isUIVisible)
+            if (isRecordUIVisible)
             {
-                recordUI.SetActive(false);
-                isUIVisible = false;
+                recordUI.SetActive(false);            
+                isRecordUIVisible = false;
+            }
+            else if(isSettingUIVisible)
+            {
+                settingUI.SetActive(false);
+                isSettingUIVisible = false;
             }
         }
+
+        
     }
 }
