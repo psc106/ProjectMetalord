c587d98a8d3a3a1a4e31a27ba6a3843d58729955
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-11 11:54:06 +0900

    배경택:[Add] 타이틀화면 기초 제작

---
+++ Metalord/Assets/_Test/BKT/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Metalord/Assets/_Test/BKT/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject anykeyDisplay; //최초 아무키 누르면 사라지는 디스플레이
+    [SerializeField] private GameObject titleDisplay; // 타이틀 화면
+    [SerializeField] private GameObject gameStartDisplay; // 게임 시작을 위한 화면
+    [SerializeField] private GameObject settingDisplay; // 환경 설정을 위한 화면
+    [SerializeField] private GameObject explainDisplay; // 게임 설명 화면
+
+    private bool isPushAnyKey = false;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && !isPushAnyKey) //아무키나 누르면 타이틀 화면이 나옴
+        {
+            isPushAnyKey = true;
+            anykeyDisplay.SetActive(false);
+            titleDisplay.SetActive(true);
+        }
+    }
+
+    // 시작 버튼 누를 시
+    public void onStartButton()
+    {
+        titleDisplay.SetActive(false);
+        gameStartDisplay.SetActive(true);
+    }
+
+    // 환경설정 버튼 누를 시
+    public void onSettingsButton()
+    {
+        titleDisplay.SetActive(false);
+        settingDisplay.SetActive(true);
+    }
+
+    // 설명 버튼 누를 시
+    public void onExplainButton()
+    {
+        titleDisplay.SetActive(false);
+        explainDisplay.SetActive(true);
+
+    }
+
+    // 나가기 버튼 게임 종료
+    public void onExitButton()
+    {
+        Application.Quit();
+    }
+
+    // 타이틀화면으로 돌아가기
+    public void backTitle()
+    {
+        // 타이틀 화면 On
+        titleDisplay.SetActive(true);
+
+        // 나머지 화면 Off
+        gameStartDisplay.SetActive(false);
+        settingDisplay.SetActive(false);
+        explainDisplay.SetActive(false);
+    }
+}
