e9a547f60d40460d4400eaf9f9b786f0aba5ee88
psc106 <42193234+psc106@users.noreply.github.com>
2023-12-13 20:01:39 +0900

    박성철 [Refactor] 플레이어 수치 조정

--- Metalord_btin/MetaLord/Assets/_Test/PSC/Scripts/Player/CameraManager.cs
+++ Metalord_btin/MetaLord/Assets/_Test/PSC/Scripts/Player/CameraManager.cs
@@ -21,14 +21,14 @@ public class CameraManager : MonoBehaviour
     [SerializeField, Range(0.5f, 20f)] float SpeedMulitiplier = 1f;
 
     bool isUnLockPressed = false;
-    bool cameraMovementLock = false;
+    static public bool cameraMovementLock { get; private set; }
 
     float newRotationY;
     float newRotationX;
 
-
     private void OnEnable()
     {
+        cameraMovementLock = false;
         transform.parent = null;
 
         input.Look += OnLook;
@@ -51,13 +51,14 @@ public class CameraManager : MonoBehaviour
     private void LateUpdate()
     {
 
+        if (cameraMovementLock) return;
+
         if (input.mouseMovement.magnitude == 0) 
         {
             newRotationY = targetX.eulerAngles.y;
             newRotationX = targetX.eulerAngles.x;
             return;
         }
-
         climbCamera.gameObject.SetActive(player.OnClimb);
         crossHair.gameObject.SetActive(!player.OnClimb);
         //Vector2 cameraMovement = input.mouseMovement;
@@ -96,15 +97,13 @@ public class CameraManager : MonoBehaviour
             {
                 newRotationY = Mathf.Clamp(newRotationY, (anchor - 89), (anchor + 89));
             }
-            targetY.transform.eulerAngles = new Vector3(0, newRotationY, 0);
         }
-        else
-        {
-            targetY.transform.eulerAngles = new Vector3(0, newRotationY, 0);
-        }
+        targetY.transform.eulerAngles = new Vector3(0, newRotationY, 0);
+        //targetY.transform.eulerAngles = new Vector3(0, Mathf.Lerp(targetY.transform.eulerAngles.y, newRotationY, 15 * Time.deltaTime), 0);
 
         //y축 변경
         //x축 변경
+        //targetX.rotation = Quaternion.Euler(newRotationX, Mathf.Lerp(targetX.eulerAngles.y, newRotationY, 15 * Time.deltaTime), targetX.eulerAngles.z);
         targetX.rotation = Quaternion.Euler(newRotationX, newRotationY, targetX.eulerAngles.z);
     }
 
@@ -120,6 +119,7 @@ public class CameraManager : MonoBehaviour
 
     void OnEnableMouseControlCamera()
     {
+        if (cameraMovementLock) return;
         isUnLockPressed = true;
 
         Cursor.lockState = CursorLockMode.None;
@@ -127,19 +127,20 @@ public class CameraManager : MonoBehaviour
     }
     void OnDisableMouseControlCamera()
     {
+        if (cameraMovementLock) return;
         isUnLockPressed = false;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        StartCoroutine(DisableMouseForFrame());
     }
 
-    IEnumerator DisableMouseForFrame()
+    public static void SwitchCameraLock(bool check)
     {
-        cameraMovementLock = true;
-        yield return new WaitForEndOfFrame();
-        cameraMovementLock = false;
+        cameraMovementLock = check;
+        if(check) Cursor.lockState = CursorLockMode.None;
+        else Cursor.lockState = CursorLockMode.Locked;
     }
+
 
 }
