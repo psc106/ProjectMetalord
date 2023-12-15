6c907a48579d18bf3bfbeb92ea24037bed5ff78c
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-15 19:43:28 +0900

    배경택:[Add] 오디오 매니저 생성, 임시 오디오 추가

---
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 오디오 매니저
+/// 231215 배경택
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    [Header("오디오 소스")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("오디오 클립")]
+    [Tooltip("필요한 오디오 클립을 인스펙터창에서 넣어주면 됩니다.")]
+    public AudioClip clip_Background;
+    public AudioClip clip_Getcoin;
+    public AudioClip clip_Jump;
+
+    private void Start()
+    {
+        PlayBackGroundMusic(); // 최초 음악시작 -> 음악시작위치 조정 필요시 위치 변경
+    }
+
+    // 최초 음악 시작 시점
+    public void PlayBackGroundMusic()
+    {
+        musicSource.clip = clip_Background;
+        musicSource.Play();
+    }
+
+    // 배경 음악 변경시
+    public void ChangeBackGroundMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+    }
+
+    // 효과음 실행시
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+
+}
