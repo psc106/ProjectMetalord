4f21e70786163615a544ee11bcced0a9f0abb218
DESKTOP-4UMPI50\KGA <tkdtkddmlehdanf97@gmail.com>
2023-12-19 21:04:31 +0900

    김형준 : [Add] mesh combiner 테스트 추가

---
+++ Metalord_btin/MetaLord/Assets/_Test/KHJ/Scripts/MeshCombine/MeshCombiner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+    [SerializeField] private List<MeshFilter> sourceMeshFilters;
+    [SerializeField] private MeshFilter targetMeshFilter;
+
+    [ContextMenu("Combine Meshes")]
+    private void CombineMeshes()//PleaseDontSayYouAreLazy()
+    {
+        var combine = new CombineInstance[sourceMeshFilters.Count];
+
+        for (var i = 0; i < sourceMeshFilters.Count; i++)
+        {
+            combine[i].mesh = sourceMeshFilters[i].sharedMesh; // sharedMesh 가 뭘까요?
+            combine[i].transform = sourceMeshFilters[i].transform.localToWorldMatrix; //로컬에서 월드 매트릭스로 ? Crazy
+        }
+
+        var mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        mesh.CombineMeshes(combine);
+        targetMeshFilter.mesh = mesh;
+    }
+
+
+
+}
