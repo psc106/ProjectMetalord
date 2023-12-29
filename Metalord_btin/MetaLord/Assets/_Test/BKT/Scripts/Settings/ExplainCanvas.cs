77ac8f9a79e8f5db9b9fc0f1895fdf92db7a3ed7
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-29 15:37:00 +0900

    배경택:[Add] 도움말 UI 및 스크립트 수정 완료

--- Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/Settings/ExplainCanvas.cs
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/Settings/ExplainCanvas.cs
@@ -2,84 +2,107 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum ExplainPage
+{
+    Panel = 0,
+    FirstKey,
+    PC,
+    Shoot,
+    Move,
+    Coin,
+    Upgrade,
+    GetItem,
+    Record,
+    NPC,
+    NOTE,
+}
+
 /// <summary>
 /// 설명 창
 /// 231226 배경택
 /// </summary>
 public class ExplainCanvas : MonoBehaviour
 {
+    private bool isFirstOpen;
 
-    private int pageIndex = 0;
-    [SerializeField] private GameObject leftButton;
-    [SerializeField] private GameObject rightButton;
+    private int _pageIndex;
+    private int pageIndex
+    {
+        get
+        {
+            return _pageIndex;
+        }
 
-    const int FIRST_IMAGE_INDEX = 1;
-    const int LAST_IMAGE_INDEX = 4;
+        set
+        {
+            transform.GetChild(_pageIndex).gameObject.SetActive(false);
+            _pageIndex = value;
+            transform.GetChild(_pageIndex).gameObject.SetActive(true);
+        }
+    }
 
+    private void Awake()
+    {
+        isFirstOpen = true;
+    }
 
     private void OnEnable()
     {
-        transform.GetChild(1).gameObject.SetActive(true);
-        transform.GetChild(2).gameObject.SetActive(false);
-        transform.GetChild(3).gameObject.SetActive(false);
-        transform.GetChild(4).gameObject.SetActive(false);
-
-        pageIndex = FIRST_IMAGE_INDEX;
-        buttonCheck();
-    }
-
-    //좌측버튼 클릭
-    public void ClickLeftButton()
-    {
-        pageIndex--;
-        for(int i = FIRST_IMAGE_INDEX; i <= LAST_IMAGE_INDEX; i++)
+        if (!isFirstOpen)
         {
-            transform.GetChild(i).gameObject.SetActive(false);
-        }
-
-        transform.GetChild(pageIndex).gameObject.SetActive(true);
-
-        buttonCheck();
-    }
-
-    //우측버튼 클릭
-    public void ClickRightButton()
-    {
-        pageIndex++;
-        for (int i = FIRST_IMAGE_INDEX; i <= LAST_IMAGE_INDEX; i++)
-        {
-            transform.GetChild(i).gameObject.SetActive(false);
-        }
-
-        transform.GetChild(pageIndex).gameObject.SetActive(true);
-
-        buttonCheck();
-    }
-
-    // 버튼 체크
-    private void buttonCheck()
-    {
-        CanUseSound();
-
-        if (pageIndex == FIRST_IMAGE_INDEX) // 첫 페이지
-        {
-            leftButton.SetActive(false);
-            rightButton.SetActive(true);
-        }
-        else if (pageIndex == LAST_IMAGE_INDEX) // 가장 끝 페이지
-        {
-            leftButton.SetActive(true);
-            rightButton.SetActive(false);
+            pageIndex = (int)ExplainPage.PC;
+            transform.GetChild((int)ExplainPage.NOTE).gameObject.SetActive(true);            
         }
         else
         {
-            leftButton.SetActive(true);
-            rightButton.SetActive(true);
+            isFirstOpen = false;
+            pageIndex = (int)ExplainPage.FirstKey;
+            transform.GetChild((int)ExplainPage.Panel).gameObject.SetActive(true);
         }
     }
 
+    public void Click_PC()
+    {
+        pageIndex = (int)ExplainPage.PC;        
+    }
+
+    public void Click_Shoot()
+    {
+        pageIndex = (int)ExplainPage.Shoot;
+    }
+
+    public void Click_Move()
+    {
+        pageIndex = (int)ExplainPage.Move;
+    }
+
+    public void Click_Coin()
+    {
+        pageIndex = (int)ExplainPage.Coin;
+    }
+
+    public void Click_Upgrade()
+    {
+        pageIndex = (int)ExplainPage.Upgrade;
+    }
+
+    public void Click_GetItem()
+    {
+        pageIndex = (int)ExplainPage.GetItem;
+    }
+
+    public void Click_Record()
+    {
+        pageIndex = (int)ExplainPage.Record;
+    }
+
+    public void Click_NPC()
+    {
+        pageIndex = (int)ExplainPage.NPC;
+    }
+
     // 임시 소리
-    private void CanUseSound()
+    public void CanUseSound()
     {
         // 사운드 추가       
         SoundManager.instance.PlaySound(GroupList.UI, (int)UISoundList.Can_BuySound);
