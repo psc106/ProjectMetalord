c7d186d173ce382505cadf1f0712e24ce1385e67
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-02 17:55:23 +0900

    배경택:[Unity] 스크립트 폴더 정리

---
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Save/PlayerData.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public Vector3 pos;
+    public Vector3 rotation;
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.dataEvents.onSaveData += SaveObject;
+        GameEventsManager.instance.dataEvents.onLoadData += LoadObject;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.dataEvents.onSaveData -= SaveObject;
+        GameEventsManager.instance.dataEvents.onLoadData -= LoadObject;
+    }
+
+    // 오브젝트 저장
+    public void SaveObject()
+    {
+        pos = transform.position;
+        rotation = transform.eulerAngles;
+
+        string jsonData = JsonUtility.ToJson(GetComponent<PlayerData>());   // 저장할 Json Data        
+
+        DataManager.instance.savedGamePlayData.playerTransform = jsonData;
+
+    }
+
+    // 오브젝트 불러오기
+    public void LoadObject()
+    {
+        string jsonData = DataManager.instance.savedGamePlayData.playerTransform;// 불러올 Json Data
+
+        JsonUtility.FromJsonOverwrite(jsonData, GetComponent<PlayerData>()); // json 파일 덮어쓰기
+
+        GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+
+        transform.position = pos;
+        transform.eulerAngles = rotation;
+
+        //StartCoroutine(PlayerRigidbody());
+
+        //Debug.Log($"{pos.x} + {pos.y} + {pos.z}");
+        //Debug.Log($"{rotation.x} + {rotation.y} + {rotation.z}");
+        //Debug.Log("값이 변경 됬나 ?");
+    }
+
+    //IEnumerator PlayerRigidbody()
+    //{
+    //    yield return null;
+    //    GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.Interpolate;
+    //}
+}
---
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Settings/DisplayMode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 디스플레이 셋팅(전체화면, 창화면)
+/// 240102 배경택
+/// </summary>
+public class DisplaySetting : MonoBehaviour
+{
+    
+}
