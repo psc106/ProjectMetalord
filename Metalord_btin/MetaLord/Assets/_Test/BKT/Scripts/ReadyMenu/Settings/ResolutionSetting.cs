0e5a0f9827bdfcbf9be6fdfee536e8195b370e82
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-03 17:01:55 +0900

    배경택:[Add] 화면 해상도 4가지로 변경 (16:9 비율)

--- Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Settings/ResolutionSetting.cs
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Settings/ResolutionSetting.cs
@@ -12,27 +12,30 @@ public class ResolutionSetting : MonoBehaviour
 {
     [SerializeField] private DisplaySetting displaySetting; // 화면모드를 가져올 변수
 
-    private Resolution[] resolutions; // 해상도를 담을 변수
+    //private Resolution[] resolutions; // 해상도를 담을 변수
     private List<string> str_resolution = new List<string>();
     private TMP_Text resText; // 해상도 표현 텍스트
     private int resIndex; // 해상도를 가리킬 인덱스값
 
+    private Vector2Int[] resolutionXY = new Vector2Int[] { new Vector2Int (1920,1080), new Vector2Int(1600, 900), new Vector2Int(1360, 768), new Vector2Int(1280, 720)};
+
     private void Awake()
     {
-        resText = transform.GetChild(0).GetComponent<TMP_Text>(); // 글자 표시 텍스트        
+        resText = transform.GetChild(0).GetComponent<TMP_Text>(); // 글자 표시 텍스트
+        resIndex = 0;                                                                 
 
     }
 
     private void Start()
     {
-        resolutions = Screen.resolutions;
+        //resolutions = Screen.resolutions;
 
-        for(int i = 0; i < resolutions.Length; i++)
+        for(int i = 0; i < resolutionXY.Length; i++)
         {
-            string temp = resolutions[i].width + "x" + resolutions[i].height;
+            string temp = resolutionXY[i].x + "x" + resolutionXY[i].y;
             str_resolution.Add(temp);
 
-            if(Screen.currentResolution.width == resolutions[i].width && Screen.currentResolution.height == resolutions[i].height)
+            if(Screen.currentResolution.width == resolutionXY[i].x && Screen.currentResolution.height == resolutionXY[i].y)
             {
                 resIndex = i;
             }
@@ -50,19 +53,19 @@ public class ResolutionSetting : MonoBehaviour
             resIndex--;
 
             InputText(resIndex);
-            Screen.SetResolution(resolutions[resIndex].width, resolutions[resIndex].height,Convert.ToBoolean(displaySetting.isFullScreen));
+            Screen.SetResolution(resolutionXY[resIndex].x,resolutionXY[resIndex].y, Convert.ToBoolean(displaySetting.isFullScreen));
         } 
     }
 
     // 우측버튼 누를경우
     public void PushRight()
     {
-        if (resIndex < resolutions.Length)
+        if (resIndex < resolutionXY.Length-1)
         {
             resIndex++;
 
             InputText(resIndex);
-            Screen.SetResolution(resolutions[resIndex].width, resolutions[resIndex].height, Convert.ToBoolean(displaySetting.isFullScreen));
+            Screen.SetResolution(resolutionXY[resIndex].x, resolutionXY[resIndex].y, Convert.ToBoolean(displaySetting.isFullScreen));
         }
 
     }
