d929b2b482fe1e2e2dff085051a7ed22e2c6333d
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-08 15:24:39 +0900

    배경택:[Fix] 게임 재시작시 Audio OnOff 오류 수정

--- Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Settings/VolumeSetting.cs
+++ Metalord_btin/MetaLord/Assets/_Test/BKT/Scripts/ReadyMenu/Settings/VolumeSetting.cs
@@ -59,6 +59,9 @@ public class VolumeSetting : MonoBehaviour
         toggle_BGM.onValueChanged.AddListener(SetControlBGM);
         toggle_SFX.onValueChanged.AddListener(SetControlSFX);
 
+        bgm_Audio = SoundManager.instance.transform.GetChild(0).GetComponent<AudioSource>();
+        sfx_Audio = SoundManager.instance.transform.GetChild(1).GetComponent<AudioSource>();
+
 
     }
 
