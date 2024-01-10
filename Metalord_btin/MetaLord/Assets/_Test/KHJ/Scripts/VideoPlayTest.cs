f72138c9bbeb6fd52497bab222f6a14e24056d4c
DESKTOP-4UMPI50\KGA <tkdtkddmlehdanf97@gmail.com>
2024-01-10 16:03:38 +0900

    김형준 : [Feat] 엔딩 크레딧 애니메이션 추가

--- Metalord_btin/MetaLord/Assets/_Test/KHJ/Scripts/VideoPlayTest.cs
+++ Metalord_btin/MetaLord/Assets/_Test/KHJ/Scripts/VideoPlayTest.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class VideoPlayTest : MonoBehaviour
 {
-    public GameObject test;
+    [SerializeField] private GameObject videoPlayer;
+    [SerializeField] private AudioSource BGMSource;
+    [SerializeField] private AudioSource SFXSource;
 
-    void Start()
+    float save_BGMVolume;
+    float save_SFXVolume;
+
+    public void Start()
     {
-        test.SetActive(false);
+        save_BGMVolume = BGMSource.volume;
+        save_SFXVolume = SFXSource.volume;
+        BGMSource.volume = 0.3f;
+        SFXSource.volume = 0.3f;
     }
 
     public void TestPlayVideo()
     {
-        test.SetActive(true);
+        videoPlayer.SetActive(true);
+        BGMSource.mute = true;
+        SFXSource.mute = true;
+
     }
 
     public void OffCanvas()
     {
+       StartCoroutine(OffGame());
+
+    }
+
+    IEnumerator OffGame() 
+    {
+        yield return new WaitForSeconds(2f);
         gameObject.SetActive(false);
+        BGMSource.mute = false;
+        SFXSource.mute = false;
+        BGMSource.volume = save_BGMVolume;
+        SFXSource.volume = save_SFXVolume;
     }
 }
